23ae96e871f9fe7a032dc9eeff04bdedc19d55c6
Mark Thomas <markthethomas@gmail.com>
2016-04-07 09:29:25 -0700

    stuff

--- steps/2/bar.spec.js
+++ steps/2/bar.spec.js
@@ -35,7 +35,7 @@ test('Asking the object where it came from (heritage)', t => {
   t.true(Drink.isPrototypeOf(Cidre));
 });
 
-test('Inspeecting object properties', t => {
+test('Inspecting object properties', t => {
   const apfelwine = new Cidre(12, 'apfelwine');
   t.is(apfelwine.name, 'apfelwine');
   t.is(apfelwine.remainingOunces, 12);
